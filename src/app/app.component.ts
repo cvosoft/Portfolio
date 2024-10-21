7157b65357b5f518c99ebfbeb1672b1926f6c61a
Christoph Völker <christoph@herrvoelker.de>
2024-10-21 12:11:07 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
