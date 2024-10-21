01266c16dc8c8dc60544871514497641f6e371bd
Christoph Völker <christoph@herrvoelker.de>
2024-10-21 16:29:28 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'legalnotice', component: LegalNoticeComponent },
+    { path: 'privacypolicy', component: PrivacyPolicyComponent }
+];
