7a8f2cdecd0aeeb2a5cfa4db3a103f97adcc404f
Christoph Völker <christoph@herrvoelker.de>
2024-10-28 11:47:05 +0100

    update

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -9,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class MySkillsComponent {
 
+  skillsList = [
+    {
+      name: "Angular",
+      img: "ang.svg"
+    },
+    {
+      name: "TypeScript",
+      img: "ts.svg"
+    },
+    {
+      name: "JavaScript",
+      img: "js.svg"
+    },
+    {
+      name: "HTML",
+      img: "html.svg"
+    },
+    {
+      name: "CSS",
+      img: "css.svg"
+    },
+    {
+      name: "Rest-Api",
+      img: "rest.svg"
+    },
+    {
+      name: "Firebase",
+      img: "fire.svg"
+    },
+    {
+      name: "GIT",
+      img: "git.svg"
+    },
+    {
+      name: "Scrum",
+      img: "scrum.svg"
+    },
+    {
+      name: "Material Design",
+      img: "matdes.svg"
+    },
+    {
+      name: "Challenge me",
+      img: "chall.svg"
+    }
+  ];
+
+
 }
