52e1b689ec7be9a58859e60380017b49a99ae27b
Christoph Völker <christoph@herrvoelker.de>
2024-10-30 17:28:41 +0100

    update

--- src/app/main-content/hero-section/nav-bar/nav-bar.component.ts
+++ src/app/main-content/hero-section/nav-bar/nav-bar.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../../../shared/logo/logo.component";
+import { TranslateModule } from "@ngx-translate/core";   // <--- standalone only
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, TranslateModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+
 
 }
