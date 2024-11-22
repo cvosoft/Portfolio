28109d48a3e25c8d06f45a4cedb175477925f46c
Christoph Völker <christov@gmx.de>
2024-11-22 10:29:44 +0100

    legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "./../main-content/hero-section/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
