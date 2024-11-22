3a7aeed5532f2c93498922956723a1948877f513
Christoph Völker <christov@gmx.de>
2024-11-22 11:30:24 +0100

    update

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "./../main-content/hero-section/nav-bar/nav-bar.component";
+import { TranslateModule } from "@ngx-translate/core";   // <--- standalone only
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,TranslateModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+
+  constructor(public translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+    //console.log(this.translate.currentLang);
+  }
 
 }
