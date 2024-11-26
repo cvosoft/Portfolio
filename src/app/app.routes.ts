a5a22785aa6f705f11d7fb6891583e687f48d085
Christoph Völker <christov@gmx.de>
2024-11-26 09:57:05 +0100

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
-import { Routes } from '@angular/router';
+import { ExtraOptions, Routes } from '@angular/router';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainContentComponent } from './main-content/main-content.component';
+
+const extraOptions: ExtraOptions = {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+};
+
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LogoComponent } from "../../shared/logo/logo.component"
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LogoComponent, TranslateModule],
+  imports: [LogoComponent, TranslateModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+
+
 
 }
