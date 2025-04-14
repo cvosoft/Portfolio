b698ca6d0566c0c14d996d061bcc18ce9275e1b8
Christoph Völker <christov@gmx.de>
2025-04-14 22:06:48 +0200

    cv stuff

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -7,12 +7,13 @@ import { MyProjectsComponent } from "./my-projects/my-projects.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { NavBarComponent } from "./hero-section/nav-bar/nav-bar.component";
 import { TranslateService } from '@ngx-translate/core';
+import { CvComponent } from "./cv/cv.component";
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavBarComponent, HeroSectionComponent, ContactComponent, WhyMeComponent, ReferencesComponent, MyProjectsComponent, MySkillsComponent],
+  imports: [NavBarComponent, HeroSectionComponent, ContactComponent, WhyMeComponent, CvComponent, MyProjectsComponent, MySkillsComponent, CvComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
